5bfb550fcd1406a2daf08694f985729ba58289c9
kecy nji <njingu90@gmail.com>
2021-11-11 03:22:25 +0000

    first_commit

---
+++ file3.py
@@ -0,0 +1,16 @@
+#demo gitnore file
+node{
+stage("gitclone"){
+    git credentialsId: 'git-cred', url: 'https://github.com/njingu90/myapp-ansible.git'
+}
+stage("deploy-dev"){
+    ansiblePlaybook credentialsId: 'digitalocean-1', disableHostKeyChecking: true, installation: 'ansible2', inventory: 'dev.inv', playbook: 'apache.yml'
+}
+stage("manaul-approval"){
+    timeout(time:8, unit:'MINUTES'){
+   input message: 'please verify and approve to deploy'
+}}
+stage("deploy-prod"){
+   ansiblePlaybook credentialsId: 'digitalocean-1', disableHostKeyChecking: true, installation: 'ansible2', inventory: 'prod.inv', playbook: 'apache.yml'
+}
+}
