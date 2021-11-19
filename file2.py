498cda80ca518d7d8839fed07c1f538128638e2a
EC2 Default User <ec2-user@ip-172-31-9-146.ec2.internal>
2021-11-19 23:18:56 +0000

    updated file2.py

--- file2.py
+++ file2.py
@@ -1,16 +1,17 @@
-#sample cicd with ansible
-node{
-stage("gitclone"){
-    git credentialsId: 'git-cred', url: 'https://github.com/njingu90/myapp-ansible.git'
-}
-stage("deploy-dev"){
-    ansiblePlaybook credentialsId: 'digitalocean-1', disableHostKeyChecking: true, installation: 'ansible2', inventory: 'dev.inv', playbook: 'apache.yml'
-}
-stage("manaul-approval"){
-    timeout(time:8, unit:'MINUTES'){
-   input message: 'please verify and approve to deploy'
-}}
-stage("deploy-prod"){
-   ansiblePlaybook credentialsId: 'digitalocean-1', disableHostKeyChecking: true, installation: 'ansible2', inventory: 'prod.inv', playbook: 'apache.yml'
-}
-}
+terraform {
+          required_providers {
+                  aws = {
+                            source  = "hashicorp/aws"
+                                  version = "~> 3.0"
+                                      }
+                    }
+          }
+
+# Configure the AWS Provider
+provider "aws" {
+          region = "us-east-1"
+          }
+
+# Create a VPC
+resource "aws_vpc" "example" {
+          cidr_block = "10.0.0.0/16"
