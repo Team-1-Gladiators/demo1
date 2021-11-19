bd57713151ec201754d86697d5dd9621717dd602
njingu90 <85954427+njingu90@users.noreply.github.com>
2021-11-19 17:28:58 -0600

    update file2.py
    addded ebs instead

--- file2.py
+++ file2.py
@@ -1,17 +1,8 @@
-terraform {
-          required_providers {
-                  aws = {
-                            source  = "hashicorp/aws"
-                                  version = "~> 3.0"
-                                      }
-                    }
-          }
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 40
 
-# Configure the AWS Provider
-provider "aws" {
-          region = "us-east-1"
-          }
-
-# Create a VPC
-resource "aws_vpc" "example" {
-          cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
